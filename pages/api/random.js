49202343b031fd61770ae931bd7244834714ef9a
Jesús Roldán <jesus.roldan@gmail.com>
2020-04-28 12:42:57 +0200

    tesy

--- pages/api/random.js
+++ pages/api/random.js
@@ -1,6 +1,6 @@
 // Fake users data
 const codes = [200, 204, 301, 302, 304, 307, 308, 400, 401, 403, 404, 408, 410, 429, 500, 502, 503, 504];
-var code = codes[Math.floor(Math.random() * codes.length)];
+const code = codes[Math.floor(Math.random() * codes.length)];
 
 export default (req, res) => {
   res.status(code).json({
