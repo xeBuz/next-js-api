15b8639617dd4a8c455b9ecae2da7eb99603b38c
Jesús Roldán <jesus.roldan@gmail.com>
2020-04-28 12:40:07 +0200

    Fix

--- pages/api/random.js
+++ pages/api/random.js
@@ -1,7 +1,6 @@
 // Fake users data
 const codes = [200, 204, 301, 302, 304, 307, 308, 400, 401, 403, 404, 408, 410, 429, 500, 502, 503, 504];
-const code = codes[Math.floor(Math.random() * items.length)];
-
+var code = codes[Math.floor(Math.random() * codes.length)];
 
 export default (req, res) => {
   res.status(code).json({
