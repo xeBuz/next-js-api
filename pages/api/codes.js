bec44ba1b91cc13de56b4c1a351ad0e6b409b9db
Jesús Roldán <jesus.roldan@gmail.com>
2020-04-28 11:48:06 +0200

    404

--- pages/api/codes.js
+++ pages/api/codes.js
@@ -1,7 +1,4 @@
 // Fake users data
-const codes = [{ id: 1 }, { id: 2 }, { id: 3 }]
-
 export default (req, res) => {
-  // Get data from your database
-  res.status(200).json(codes);
+  res.status(404).json({});
 }
