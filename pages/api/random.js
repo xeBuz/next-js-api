5159fc69105fc33da25334d9987e40fe40629211
Jesús Roldán <jesus.roldan@gmail.com>
2020-04-28 12:34:16 +0200

    Added Random

---
+++ pages/api/random.js
@@ -0,0 +1,10 @@
+// Fake users data
+const codes = [200, 204, 301, 302, 304, 307, 308, 400, 401, 403, 404, 408, 410, 429, 500, 502, 503, 504];
+const code = codes[Math.floor(Math.random() * items.length)];
+
+
+export default (req, res) => {
+  res.status(code).json({
+    'code': code
+  });
+}
