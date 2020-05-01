9ae55468adced5d0841eeb7e4765ab734aea43f0
Jesús Roldán <jesus.roldan@gmail.com>
2020-05-01 10:04:58 +0200

    Cleanup

--- pages/api/random.js
+++
@@ -1,9 +0,0 @@
-// Fake users data
-const codes = [200, 204, 301, 302, 304, 307, 308, 400, 401, 403, 404, 408, 410, 429, 500, 502, 503, 504];
-const code = codes[Math.floor(Math.random() * codes.length)];
-
-export default (req, res) => {
-  res.status(code).json({
-    'code': code
-  });
-}
--- pages/index.js
+++ pages/index.js
@@ -6,18 +6,9 @@ const fetcher = url => fetch(url).then(res => res.json())
 export default function Index() {
   const { data, error } = useSwr('/api/users', fetcher)
 
-  if (error) return <div>Failed to load users</div>
-  if (!data) return <div>Loading...</div>
-
   return (
-    <ul>
-      {data.map(user => (
-        <li key={user.id}>
-          <Link href="/user/[id]" as={`/user/${user.id}`}>
-            <a>{`User ${user.id}`}</a>
-          </Link>
-        </li>
-      ))}
-    </ul>
+    <div>
+      <p>Try the API</p>
+    </div>
   )
 }
