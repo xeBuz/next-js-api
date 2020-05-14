d42767613fd31db8490a7303123e47f8adb5dfa0
Jesús Roldán <jesus.roldan@gmail.com>
2020-05-14 16:17:33 +0200

    Fetch from file

--- pages/api/links/[link].js
+++ pages/api/links/[link].js
@@ -1,35 +1,22 @@
+const fetch = require("node-fetch");
+
 export default async (req, res) => {
     const { query: { link } } = req
     var code = 404;
 
-    const urls = [
-        {link: "example", code: 404},
-        {link: "ok", code: 200},
-        {link: "testing", code: 200},
-        {link: "qa", code: 500},
-        {link: "cq", code: 503},
-        {link: "miss", code: 404},
-        {link: "again", code: 404},
-        {link: "blue", code: 200},
-        {link: "red", code: 200},
-        {link: "yellow", code: 200},
-        {link: "pink", code: 200},
-        {link: "orange", code: 200},
+    await fetch("https://jsonblob.com/api/jsonBlob/075ff1c4-95ec-11ea-87c3-0f1e1613ada4")
+        .then(response => response.json())
+        .then(links => {
 
-        {link: "frodo", code: 403},
-        {link: "bilbo", code: 403},
-        {link: "sam", code: 403},
-        {link: "merry", code: 403},
-        {link: "pippin", code: 403},
-    ];
+            var existing = links.data.find(u => u.link.toLowerCase() == link.toLowerCase());
 
-    var existing = urls.find(u => u.link.toLowerCase() == link.toLowerCase())
-    if (existing) {
-        code = existing.code;
-    }
+            if (existing) {
+                code = existing.code;
+            }
 
-    res.status(code).json({
-        'code': code,
-        'link': link
-      });
+            res.status(code).json({
+                'code': code,
+                'link': link
+              });
+        });
   }
